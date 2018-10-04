01dbba86ade0979fccb97ddbaf2aff1649945242
Sara Hansson <sarahansson@Saras-MacBook-Pro.local>
2018-10-04 10:59:06 +0200

    Click button to change background color

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("hello world");
+function changeBackground() {
+
+    if  (document.body.style.backgroundColor == "white") {
+        document.body.style.backgroundColor = ("blue");
+    } else {
+        document.body.style.backgroundColor = "white";
+    }
+    
+}
+
+// Pass by value:
+//string, number, boolean
+
+//Pass by reference: 
+// array, object
+
+var footballPlayer = {
+    name: "David Jensen",
+    age: 22,
+    shirtNumber: 14,
+    position: "Mid-fielder",
+    gender: "male",
+
+}
+
+var newFootballPlayer = footballPlayer;
+newFootballPlayer.name = "Albert Einstein";
+console.log(newFootballPlayer);
+console.log(footballPlayer);
+
+var message = "Welcome";
+var newMessage = message;
+newMessage = "Welcome home!";
+console.log(message);
+console.log(newMessage);
